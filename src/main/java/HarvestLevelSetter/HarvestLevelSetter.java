a6c77e780f504c453c20c095a84b67d007eb1fb9
aksource <akira.mew@gmail.com>
2016-07-17 22:26:37 +0900

    変更点：1.9.4及び1.10に対応。

--- src/main/java/HarvestLevelSetter/HarvestLevelSetter.java
+++ src/main/java/HarvestLevelSetter/HarvestLevelSetter.java
@@ -1,14 +1,14 @@
 package HarvestLevelSetter;
 
-import cpw.mods.fml.common.FMLLog;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameData;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.FMLLog;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import java.util.logging.Logger;
 
@@ -16,14 +16,14 @@ import java.util.logging.Logger;
         name = HarvestLevelSetter.MOD_NAME,
         version = HarvestLevelSetter.MOD_VERSION,
         dependencies = HarvestLevelSetter.MOD_DEPENDENCIES,
-        useMetadata = true)
+        useMetadata = true,
+        acceptedMinecraftVersions = HarvestLevelSetter.MOD_MC_VERSION)
 public class HarvestLevelSetter {
-
     public static final String MOD_ID = "HarvestLevelSetter";
     public static final String MOD_NAME = "HarvestLevelSetter";
     public static final String MOD_VERSION = "@VERSION@";
-    public static final String MOD_DEPENDENCIES = "required-after:FML";
-
+    public static final String MOD_DEPENDENCIES = "required-after:Forge@[12.17.0,)";
+    public static final String MOD_MC_VERSION = "[1.9,1.10.99]";
     private static String[] blockPickaxe;
     private static String[] blockShovel;
     private static String[] blockAxe;
@@ -31,13 +31,13 @@ public class HarvestLevelSetter {
     private static String[] toolShovel;
     private static String[] toolAxe;
 
-    private static final Logger LOGGER = Logger.getLogger(HarvestLevelSetter.class.getSimpleName());
+    private static final Logger LOGGER = Logger.getLogger("HarvestLevelSetter");
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         Configuration config = new Configuration(event.getSuggestedConfigurationFile());
         config.load();
-        blockPickaxe = config.get(Configuration.CATEGORY_GENERAL, "blockPickaxe", new String[]{}, "Harvet Pickaxe Block Name List Format(No Space):BlockUniqueName,HarvestLevel,meta or BlockUniqueName,HarvestLevel").getStringList();
+        blockPickaxe = config.get(Configuration.CATEGORY_GENERAL, "blockPickaxe", new String[]{}, "Harvet Pickaxe Block Name List Format(No Space):BlockUniqueName#HarvestLevel#meta or BlockUniqueName,HarvestLevel").getStringList();
         blockShovel = config.get(Configuration.CATEGORY_GENERAL, "blockShovel", new String[]{}, "Harvet Shovel Block Name List Format(No Space):BlockUniqueName,HarvestLevel,meta or BlockUniqueName,HarvestLevel").getStringList();
         blockAxe = config.get(Configuration.CATEGORY_GENERAL, "blockAxe", new String[]{}, "Harvet Axe Block Name List Format(No Space):BlockUniqueName,HarvestLevel,meta or BlockUniqueName,HarvestLevel").getStringList();
         toolPickaxe = config.get(Configuration.CATEGORY_GENERAL, "toolPickaxe", new String[]{}, "Tool Pickaxe Name List Format(No Space):ToolUniqueName,HarvestLevel").getStringList();
@@ -64,7 +64,7 @@ public class HarvestLevelSetter {
         for (i = 0; i < toolList.length; i++) {
             toolNameAndLv = toolList[i].split(",");
             if (toolNameAndLv.length < 2) continue;
-            tool = GameData.getItemRegistry().getObject(toolNameAndLv[0]);
+            tool = Item.REGISTRY.getObject(new ResourceLocation(toolNameAndLv[0]));
             lv = getInt(toolNameAndLv[1]);
             if (tool != null && lv >= 0) {
                 tool.setHarvestLevel(toolKind, lv);
@@ -74,7 +74,7 @@ public class HarvestLevelSetter {
         for (i = 0; i < blocklist.length; i++) {
             blockNameAndLv = blocklist[i].split(",");
             if (blockNameAndLv.length < 2) continue;
-            block = GameData.getBlockRegistry().getObject(blockNameAndLv[0]);
+            block = Block.REGISTRY.getObject(new ResourceLocation(blockNameAndLv[0]));
             meta = getInt(blockNameAndLv[2]);
             lv = getInt(blockNameAndLv[1]);
             if (block != null && lv >= 0) {
@@ -82,7 +82,7 @@ public class HarvestLevelSetter {
                     block.setHarvestLevel(toolKind, lv);
                     LOGGER.fine(String.format("#Block %s %s %d", block.getLocalizedName(), toolKind, lv));
                 } else {
-                    block.setHarvestLevel(toolKind, lv, meta);
+                    block.setHarvestLevel(toolKind, lv, block.getStateFromMeta(meta));
                     LOGGER.fine(String.format("#Block %s %s %d %d", block.getLocalizedName(), toolKind, lv, meta));
                 }
             }
